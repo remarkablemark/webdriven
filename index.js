19de779d5232c4248d0c7234cf10ecb1739c99c5
Mark <mark@remarkablemark.org>
2016-11-23 13:05:10 -0500

    Expose factory in index.js that returns a helpers object
    The first parameter of the factory is the `WebDriver` instance and
    it is binded to the `findElement` and `findElements` methods.
    
    Update tests for index.js to verify that the factory returns an
    object of the helpers.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict';
 
+/**
+ * Factory for the driver-dependent helpers.
+ *
+ * @param  {WebDriver} driver - The WebDriver instance.
+ * @return {Object}           - The helpers object.
+ */
+module.exports = function(driver) {
+    return {
+        findElement: require('./lib/findElement').bind(null, driver),
+        findElements: require('./lib/findElements').bind(null, driver)
+    };
+};
+
+/**
+ * Build helper.
+ */
 module.exports.build = require('./lib/build');
--- test/index.js
+++ test/index.js
@@ -10,7 +10,16 @@ var index = require('../index');
  * Index.
  */
 describe('index', function() {
+    it('returns an object of helpers when invoked with a parameter', function() {
+        assert.deepEqual(
+            Object.keys(index(null)), [
+                'findElement',
+                'findElements'
+            ]
+        );
+    });
+
     it('exposes build', function() {
-        assert(index.hasOwnProperty('build'));
+        assert.equal(typeof index.build, 'function');
     });
 });
